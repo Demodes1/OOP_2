256b49d4db68e71ab7cef8dd99ac640520e0eef1
Guillermo Esquivel <73927126+Demodes1@users.noreply.github.com>
2022-05-30 17:42:20 -0500

    Update reto.h

--- Reto/reto.h
+++ Reto/reto.h
@@ -1,12 +1,10 @@
 using namespace std;
 
 class Video {
-    private:
+    public:  
         string tipoVideo, nombreVideo, genero;
-        int calificacion, annoLanzamiento, duracion;
-    public:        
+        int calificacion, annoLanzamiento, duracion;      
         // Constructor
-        
         Video(string _tipoVideo, string _nombreVideo, string _genero,int _duracion, int _annoLanzamiento,  int _calificacion){
             tipoVideo = _tipoVideo;
             nombreVideo = _nombreVideo;
@@ -30,4 +28,44 @@ class Video {
 
         void setMuestraDatos(string _tipoVideo, string _nombreVideo, string _genero, int _calificacion, int _annoLanzamiento, int _duracion);
         void getMuestraDatos();
+};
+
+class Pelicula: public Video {
+    public:
+        Pelicula(string _tipoVideo, string _nombreVideo, string _genero,int _duracion, int _annoLanzamiento,  int _calificacion): Video(_tipoVideo, _nombreVideo, _genero,_duracion,_annoLanzamiento,_calificacion){
+            tipoVideo = _tipoVideo;
+            nombreVideo = _nombreVideo;
+            genero = _genero;
+            annoLanzamiento = _annoLanzamiento;
+            calificacion = _calificacion;
+            duracion = _duracion;
+        }
+        
+        Pelicula(string _tipoVideo, string _nombreVideo, string _genero, int _duracion, int _annoLanzamiento):Video(_tipoVideo, _nombreVideo, _genero, _duracion, _annoLanzamiento){
+            tipoVideo = _tipoVideo;
+            nombreVideo = _nombreVideo;
+            genero = _genero;
+            annoLanzamiento = _annoLanzamiento;
+            duracion = _duracion;
+        }
+};
+
+class Serie: public Video { 
+    public:
+        Serie(string _tipoVideo, string _nombreVideo, string _genero,int _duracion, int _annoLanzamiento,  int _calificacion): Video(_tipoVideo, _nombreVideo, _genero,_duracion,_annoLanzamiento,_calificacion){
+            tipoVideo = _tipoVideo;
+            nombreVideo = _nombreVideo;
+            genero = _genero;
+            annoLanzamiento = _annoLanzamiento;
+            calificacion = _calificacion;
+            duracion = _duracion;
+        }
+
+        Serie(string _tipoVideo, string _nombreVideo, string _genero, int _duracion, int _annoLanzamiento):Video(_tipoVideo, _nombreVideo, _genero, _duracion, _annoLanzamiento){
+            tipoVideo = _tipoVideo;
+            nombreVideo = _nombreVideo;
+            genero = _genero;
+            annoLanzamiento = _annoLanzamiento;
+            duracion = _duracion;
+        }
 };
